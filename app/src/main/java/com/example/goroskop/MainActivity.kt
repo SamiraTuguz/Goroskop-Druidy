4037fa080ef1999646ce45a0406af73387597406
NataliaPopova127 <pnatala127@mail.ru>
2022-12-05 17:40:43 +0300

    add text and pictures

--- app/src/main/java/com/example/goroskop/MainActivity.kt
+++ app/src/main/java/com/example/goroskop/MainActivity.kt
@@ -1,24 +1,91 @@
 package com.example.goroskop
 
 import android.content.Intent
+import android.content.res.Configuration
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Data
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.accessibility.AccessibilityManagerCompat.TouchExplorationStateChangeListener
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_second.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    var tree : String = ""
+    var text = ""
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Goroskop)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setTitle(getString(R.string.app_name))
+
+        when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_NO -> back.background = getDrawable(R.color.back)
+            Configuration.UI_MODE_NIGHT_YES -> back.background = getDrawable(R.color.black)
+        }
+
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            text = "$dayOfMonth.${month + 1}.$year"
+        }
     }
 
     fun btnStartClick(view: View) {
-        var tree : String = ""
-        if(etDate.text.toString() == "1")
-            tree = "яблоня"
-        var intent : Intent = Intent(this@MainActivity, SecondActivity::class.java)
-        intent.putExtra("tree", tree)
-        startActivity(intent)
+        try{
+            val sdf = SimpleDateFormat("DD.MM.yyyy")
+
+            val firstDate: Date = sdf.parse(text)
+            val secondDate: Date = sdf.parse("06.09.2022")
+
+
+            Toast.makeText(this, firstDate.after(secondDate).toString(), Toast.LENGTH_LONG).show()
+            if(firstDate.before(sdf.parse("05.10.2022")) && firstDate.after(secondDate))
+                tree = "яблоня"
+            else if(firstDate.after(sdf.parse("06.10.2005")) || firstDate.before(sdf.parse("05.11.2005")))
+                tree = "кипарис"
+
+
+
+
+            var intent : Intent = Intent(this@MainActivity, SecondActivity::class.java)
+            intent.putExtra("tree", tree)
+            startActivity(intent)
+        }
+        catch (e : java.lang.Exception){
+            Toast.makeText(this,e.message, Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean{
+        when(item.itemId){
+            R.id.darkTheme -> {
+                back.background = getDrawable(R.color.black)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            R.id.lightTheme -> {
+                back.background = getDrawable(R.color.back)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/goroskop/SecondActivity.kt
+++ app/src/main/java/com/example/goroskop/SecondActivity.kt
@@ -1,12 +1,122 @@
 package com.example.goroskop
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
+    var text : String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Goroskop)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        setTitle(getString(R.string.app_name))
+        when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_NO -> back2.background = getDrawable(R.color.back)
+            Configuration.UI_MODE_NIGHT_YES -> back2.background = getDrawable(R.color.black)
+        }
+
+
+        var result : Bundle? = intent.extras
+        if(result != null){
+            text = result.getString("tree")!!
+        }
+
+        if(text == "яблоня"){
+            ivTree.setImageResource(R.drawable.yablona)
+            tvTree.text = getString(R.string.apple)
+        }
+        else if(text == "пихта"){
+            ivTree.setImageResource(R.drawable.pihta)
+            tvTree.text = getString(R.string.pihta)
+        }
+        else if(text == "вяз"){
+            ivTree.setImageResource(R.drawable.vas)
+            tvTree.text = getString(R.string.vas)
+        }
+        else if(text == "кипарис"){
+            ivTree.setImageResource(R.drawable.kiparis)
+            tvTree.text = getString(R.string.kiparis)
+        }
+        else if(text == "тополь"){
+            ivTree.setImageResource(R.drawable.topol)
+            tvTree.text = getString(R.string.topol)
+        }
+        else if(text == "кедр"){
+            ivTree.setImageResource(R.drawable.kedr)
+            tvTree.text = getString(R.string.kedr)
+        }
+        else if(text == "сосна"){
+            ivTree.setImageResource(R.drawable.sosna)
+            tvTree.text = getString(R.string.sosna)
+        }
+        else if(text == "ива"){
+            ivTree.setImageResource(R.drawable.iva)
+            tvTree.text = getString(R.string.iva)
+        }
+        else if(text == "липа"){
+            ivTree.setImageResource(R.drawable.lipa)
+            tvTree.text = getString(R.string.lipa)
+        }
+        else if(text == "орешник"){
+            ivTree.setImageResource(R.drawable.oreshnik)
+            tvTree.text = getString(R.string.oreshnik)
+        }
+        else if(text == "рябина"){
+            ivTree.setImageResource(R.drawable.rabina)
+            tvTree.text = getString(R.string.rabina)
+        }
+        else if(text == "клен"){
+            ivTree.setImageResource(R.drawable.klen)
+            tvTree.text = getString(R.string.klen)
+        }
+        else if(text == "орех"){
+            ivTree.setImageResource(R.drawable.oreh)
+            tvTree.text = getString(R.string.oreh)
+        }
+        else if(text == "жасмин"){
+            ivTree.setImageResource(R.drawable.jasmin)
+            tvTree.text = getString(R.string.jasmin)
+        }
+        else if(text == "каштан"){
+            ivTree.setImageResource(R.drawable.kashtan)
+            tvTree.text = getString(R.string.kashtan)
+        }
+        else if(text == "ясень"){
+            ivTree.setImageResource(R.drawable.asen)
+            tvTree.text = getString(R.string.asen)
+        }
+        else if(text == "граб"){
+            ivTree.setImageResource(R.drawable.grab)
+            tvTree.text = getString(R.string.grab)
+        }
+        else if(text == "инжир"){
+            ivTree.setImageResource(R.drawable.injir)
+            tvTree.text = getString(R.string.injir)
+        }
+        else if(text == "дуб"){
+            ivTree.setImageResource(R.drawable.dub)
+            tvTree.text = getString(R.string.dub)
+        }
+        else if(text == "береза"){
+            ivTree.setImageResource(R.drawable.bereza)
+            tvTree.text = getString(R.string.bereza)
+        }
+        else  if(text == "маслина"){
+            ivTree.setImageResource(R.drawable.maslina)
+            tvTree.text = getString(R.string.maslina)
+        }
+        else if(text == "бук"){
+            ivTree.setImageResource(R.drawable.buk)
+            tvTree.text = getString(R.string.buk)
+        }
+        else {
+            ivTree.setImageResource(R.drawable.logo)
+            tvTree.text = "Некорректная дата"
+        }
 
 
     }
